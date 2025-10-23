69bea5f3811dc7fde40c3cc131b37eec3f994b12
Salman4M <salmanmammadli@hotmail.com>
2025-10-23 10:10:03 +0400

    done yml

--- carts.js
+++
@@ -1,8 +0,0 @@
-"TESTING IT AGAIN ON "
-"TESTING IT AGAIN ON "
-console.log('SYNC TEST');
-"console.log('SYNC TEST');" 
-"console.log('SYNC TEST');" 
-"// SYNC TEST AFTER FIX" 
-console.log('Second sync test - everything works');
-"console.log('Second sync test - everything works!');" 
