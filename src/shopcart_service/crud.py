fd5c213f1d69619fa24754180055b930f5fbdfa1
Salman4M <salmanmammadli@hotmail.com>
2025-10-25 17:55:21 +0400

    gateway connection

--- src/shopcart_service/crud.py
+++ src/shopcart_service/crud.py
@@ -51,10 +51,10 @@ def delete_cart_item(db: Session, item_id: int,cart_id: int):
     return db_item
 
 
-def add_item_to_cart(db: Session,product_var_id: int, cart_id: int, item: schemas.CartItemCreate):
+def add_item_to_cart(db: Session,product_var_uuid: UUID4, cart_id: int, item: schemas.CartItemCreate):
     existing_item = (
         db.query(models.CartItem)
-        .filter(models.CartItem.shop_cart_id==cart_id,models.CartItem.product_variation_id==product_var_id)
+        .filter(models.CartItem.shop_cart_id==cart_id,models.CartItem.product_variation_id==product_var_uuid)
         .first()
     )
     if existing_item:
@@ -63,7 +63,7 @@ def add_item_to_cart(db: Session,product_var_id: int, cart_id: int, item: schema
         db.refresh(existing_item)
         return existing_item
     
-    db_item = models.CartItem(shop_cart_id=cart_id, product_variation_id = product_var_id)
+    db_item = models.CartItem(shop_cart_id=cart_id, product_variation_id = product_var_uuid, quantity = item.quantity)
     db.add(db_item)
     db.commit()
     db.refresh(db_item)
